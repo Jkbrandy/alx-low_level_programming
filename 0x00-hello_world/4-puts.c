71ba4fe916c718230f12311291b6a129f88670b1
jkbrandy <opey4gesa18@gmail.com>
2022-12-08 08:58:21 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * Return : Always 0 (Success)
 */
 
-int main(void)
-{ /* Returns 0 */
+int main(void) /* Returns 0 */
+{ 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
