189106d27fc35d75b5673161a835178950a948e5
jkbrandy <opey4gesa18@gmail.com>
2022-12-14 18:54:09 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the input to be checked
+ *Return: absolute n
+ */
+
+int main(void)
+{
+	int r;
+
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
